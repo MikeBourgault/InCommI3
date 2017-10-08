8077e4ccbc0d2c34848f33945c7e13b5902829ed
Michael <mike.bourgault@gmail.com>
2017-10-08 02:08:19 -0400

    More changes, also Build Successful

---
+++ app/src/main/java/com/auth0/samples/twitter_failure.java
@@ -0,0 +1,12 @@
+package com.auth0.samples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class twitter_failure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/auth0/samples/twitter_pass.java
@@ -0,0 +1,10 @@
+package com.auth0.samples;
+
+import android.app.Activity;
+
+/**
+ * Created by Michael on 10/8/2017.
+ */
+
+public class twitter_pass extends Activity {
+}
